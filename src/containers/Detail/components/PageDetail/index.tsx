7da3fc4bf615bd48d9b30b8192b54f5b29b90a05
maiiquynhh <64117178+maiiquynhh@users.noreply.github.com>
2023-08-17 13:58:51 +0700

    change size page

--- src/containers/Detail/components/PageDetail/index.tsx
+++ src/containers/Detail/components/PageDetail/index.tsx
@@ -39,7 +39,7 @@ type Props = {
 
 const PageDetail: React.FC<Props> = ({ blockMap, data }) => {
   return (
-    <div className="m-auto max-w-4xl">
+    <div className="m-auto max-w-6xl">
       <NotionRenderer
         recordMap={blockMap}
         components={{
